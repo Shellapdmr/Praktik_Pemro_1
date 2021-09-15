2b78f0be8fce3640371b779c84f606c485530fc1
Shellapdmr <shellapricillia.2021@student.uny.ac.id>
2021-09-15 16:38:07 +0900

    menambahkan komentar berupa nama dan nim

--- Pertemuan 2/Hello.java
+++ Pertemuan 2/Hello.java
@@ -1,5 +1,10 @@
+/*
+NIM : 21537149001
+Nama : Shella Pricillia Dimara
+*/
 public class Hello {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        // untuk menampilkan teks
         System.out.println("Hello world");
     }
 }
