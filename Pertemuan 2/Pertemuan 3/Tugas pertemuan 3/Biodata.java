38b9f56f6f8cf806afad2cb3e5517c16438fb638
Shellapdmr <shellapricillia.2021@student.uny.ac.id>
2021-09-16 19:35:44 +0900

    Tugas pertemuan 3

---
+++ Pertemuan 2/Pertemuan 3/Tugas pertemuan 3/Biodata.java
@@ -0,0 +1,19 @@
+public class Biodata {
+    public static void main(String[] args) {
+        String namaLengkap = "Shella Pricillia Dimara";
+        String nim = "21537149001";
+        int angkatan = 2021;
+        String programStudi = "Teknologi Informasi";
+        String jurusan = "Pendidikan Teknik Elektronika dan Informatika";
+        String fakultas = "Teknik";
+        Double ipkHarapan = 4.0;
+        System.out.println("Nama saya " + namaLengkap);
+        System.out.println("Nim saya " + nim);
+        System.out.println("Saya angkatan tahun " + (int) angkatan);
+        System.out.println("Saya dari prodi " + programStudi);
+        System.out.println("Saya dari jurusan " + jurusan);
+        System.out.println("Saya dari fakultas " + fakultas);
+        System.out.println("Ipk harapan saya adalah " + ipkHarapan);
+    
+    }
+}
---
+++ Pertemuan 2/Pertemuan 3/Tugas pertemuan 3/biodata.py
@@ -0,0 +1,14 @@
+nama = "Shella Pricillia Dimara"
+nim = 21537149001
+angkatan = 2021
+programStudi = "Teknologi Informasi"
+jurusan = "Pendidikan Teknik Elektronika dan Informatika"
+fakultas = "Teknik"
+ipkHarapan = 4.0
+print(nama)
+print(nim)
+print(angkatan)
+print(programStudi)
+print(jurusan)
+print(fakultas)
+print(ipkHarapan)
